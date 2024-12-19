7bfdc1682123f0ffeed37d60cc1cfd89f99877bf
Tinny <mos23018@byui.edu>
2024-12-19 23:12:36 +0200

    files updated

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -9,10 +9,9 @@ public class Node
         this.Data = data;
     }
 
+    // Problem 1: Insert Unique Values Only
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
         if (value < Data)
         {
             // Insert to the left
@@ -21,7 +20,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
@@ -29,17 +28,28 @@ public class Node
             else
                 Right.Insert(value);
         }
+        // If value is equal to Data, do nothing (no duplicates allowed)
     }
 
+    // Problem 2: Contains
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value < Data)
+        {
+            return Left?.Contains(value) ?? false;
+        }
+        else if (value > Data)
+        {
+            return Right?.Contains(value) ?? false;
+        }
+        return true; // Found the value
     }
 
+    // Problem 4: Get Height
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
